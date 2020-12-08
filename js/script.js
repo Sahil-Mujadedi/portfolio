16d7acf004ffd337545b3764395fee10c955300f
KillerKoop <sahil2006.com@gmail.com>
2020-12-07 18:54:39 -0700

    made header fade out with others for better transition to about page

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
 document.getElementById('start').addEventListener("click", function() {
-    slideleft("about", "start", "newGame");
+    fadeout("about", "start", "newGame");
     document.getElementById("start").innerText = "Loading...";
     setTimeout(function() {
         location.assign("files.html");
-    }, 2000);
+    }, 600);
 });
 
 document.getElementById('about').addEventListener("click", function(){
-    slideleft("about", "start", "newGame");
+    fadeout("about", "start", "newGame");
+    fadeout("name-wrapper");
     document.getElementById("about").innerText = "Loading...";
     setTimeout(function() {
         location.assign("about.html");
-    }, 2000);
+    }, 600);
 })
 
 // function to slide the buttons to the left and delete them
-function slideleft(id1, id2 = null, id3 = null) {
-    document.getElementById(id1).style = "animation: slideleft 2s linear";
+function fadeout(id1, id2 = null, id3 = null) {
+    document.getElementById(id1).style = "animation: fadeout 0.4s ease-in";
     
     setTimeout(function(){
         document.getElementById(id1).remove()
-    }, 2000);
+    }, 400);
 
     // Checks if id 1 and 2 are not null so it does not throw an error message.
     // Time out so the buttons do not immediately disappear.
     if (id2 !== null) {
-        document.getElementById(id2).style = "animation: slideleft 2s linear";
+        document.getElementById(id2).style = "animation: fadeout 0.4s ease-in;";
         setTimeout(function(){
             document.getElementById(id2).remove()
-        }, 2000);
+        }, 400);
     }
     if (id3 !== null) {
-        document.getElementById(id3).style = "animation: slideleft 2s linear";
+        document.getElementById(id3).style = "animation: fadeout 0.4s ease-in;";
         setTimeout(function(){
             document.getElementById(id3).remove()
-        }, 2000);
+        }, 400);
     }
 
 }
